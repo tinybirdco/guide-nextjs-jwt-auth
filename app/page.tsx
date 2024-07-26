09b31acac5465eb639b655245048f460b0b80e7a
sdairs <sdairs@users.noreply.github.com>
2024-07-26 15:55:05 +0100

    getting closer...

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,8 @@
 "use client";
 
 import { TopAirlines } from "./components/TopAirlines";
-import useSWR from "swr";
-import { getEndpointUrl } from "@/utils";
-import { useFetcher } from "@/hooks/useFetch";
-
-const REFRESH_INTERVAL_IN_MILLISECONDS = 5000; // five seconds
 
 export default function Dashboard() {
-  const endpointUrl = getEndpointUrl();
-  const fetcher = useFetcher(); // This fetcher handles the token revalidation
-
-  // Initializes variables for storing data
-  let top_airline, latency, errorMessage;
-
-  // Using SWR hook to handle state and refresh result every five seconds
-  const { data } = useSWR(endpointUrl, fetcher, {
-    refreshInterval: REFRESH_INTERVAL_IN_MILLISECONDS,
-    onError: (error) => (errorMessage = error),
-  });
-
-  if (!data) return;
-
-  if (data?.error) {
-    errorMessage = data.error;
-    return;
-  }
-
-  top_airline = data.data; // Setting the state with the fetched data
-  latency = data.statistics?.elapsed; // Setting the state with the query latency from Tinybird
-
   return (
     <TopAirlines />
   );
--- hooks/useFetch.tsx
+++ hooks/useFetch.tsx
@@ -2,30 +2,29 @@ import { generateJWT } from "@/server/token";
 import { useState } from "react";
 
 export function useFetcher() {
-  const [token, setToken] = useState("");
 
   // Generate a new JWT token and store the new token in the state
   const refreshToken = async () => {
     const newToken = await generateJWT();
-    setToken(newToken);
+    console.log(newToken);
+    localStorage.setItem("tinybirdJWT", newToken);
     return newToken;
   };
 
   // Function to fetch data from Tinybird URL with a JWT token
   // If the token expires, a new one is generated in the server
-  return async (url: string) => {
-    let currentToken = token;
-    if (!currentToken) {
-      currentToken = await refreshToken();
-    }
-    const response = await fetch(url + "?token=" + currentToken);
+  return async (endpoint: string) => {
+    const url = new URL(endpoint)
+
+    const response = await fetch(url);
 
     if (response.status === 200) {
       return response.json();
     }
     if (response.status === 403) {
       const newToken = await refreshToken();
-      return fetch(url + "?token=" + newToken).then((res) => res.json());
+      url.searchParams.set('token', newToken)
+      return fetch(url).then((res) => res.json());
     }
   };
 }
